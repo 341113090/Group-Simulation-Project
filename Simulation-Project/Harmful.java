392bbc2c13b3b610c86b8a6e6343d0eee89f2131
maxtan84 <87951920+maxtan84@users.noreply.github.com>
2022-04-12 10:25:06 -0400

    plant vars and getters

---
+++ Simulation-Project/Harmful.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Harmful here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Harmful extends Plant
+{
+    /**
+     * Act - do whatever the Harmful wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- Simulation-Project/Plant.java
+++ Simulation-Project/Plant.java
@@ -14,8 +14,59 @@ public class Plant extends Actor
      * Act - do whatever the Plant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    //getter required
+    protected int toughness;
+    protected int totalSeeds;
+    protected int health;
+    protected int healthGiven;
+    protected int fertility;
+    protected boolean isToxic;
+    protected boolean wantsCarry;
+    
+    //getter not needed
+    protected int healthLimit;
+    protected int rateOfGrowth;
+    protected int selfHealSpeed;
+        
     public void act()
     {
         // Add your action code here.
     }
+    
+    public void die(){
+        
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+    
+    public int giveHealth(){
+        return healthGiven;
+    }
+    
+    public int getSeeds(){
+        return totalSeeds;
+    }
+    
+    public int getToughness(){
+        return toughness;
+    }
+    
+    public boolean isToxic(){
+        return isToxic;
+    }
+    
+    public boolean wantsCarry(){
+        return wantsCarry;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
