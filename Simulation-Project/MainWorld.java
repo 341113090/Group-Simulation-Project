7974288f154e03b6aa6ea4a723d211c43cdc8b1b
frickingluwai <luwaiwong@gmail.com>
2022-04-13 10:38:25 -0400

    fix images for ethan

--- Simulation-Project/MainWorld.java
+++ Simulation-Project/MainWorld.java
@@ -6,9 +6,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+
 public class MainWorld extends World
 {
+    ///// DAY NIGHT CYCLE /////
     private int dayNumber;
+    private int currentTime;
+    private int dayLength;
+    private int nightLength;
+    
+    private Foreground fg;
+    
     private int numCherry;
     private int numPoisonIvy;
     /**
@@ -21,5 +30,30 @@ public class MainWorld extends World
         super(600, 400, 1); 
         
         addObject(new Herbivore(), 100, 100);
+        fg = new Foreground();
+        addObject(fg,0,0);
+        drawNight();
+    }
+
+    
+    public void act(){
+        
+    }
+    
+    private void timeManager(){
+        currentTime++;
+        
+        if (currentTime > dayLength){
+            
+        }
+    }
+    
+    private GreenfootImage drawNight(){
+        GreenfootImage dark = new GreenfootImage (getWidth(), getHeight());
+        dark.setColor(Color.BLACK);
+        dark.fillRect(0,0,dark.getWidth(),dark.getHeight());
+
+        fg.setImage(dark);
+        return dark;
     }
 }
