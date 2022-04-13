d2fb6ea4b344202a8b72776bff60c507132ba7f6
maxtan84 <87951920+maxtan84@users.noreply.github.com>
2022-04-13 09:51:34 -0400

    fkdsjf

--- Simulation-Project/PoisonIvey.java
+++ Simulation-Project/PoisonIvey.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Good here.
+ * Write a description of class PoisonIvey here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Good extends Plant
+public class PoisonIvey extends Plant
 {
     /**
-     * Act - do whatever the Good wants to do. This method is called whenever
+     * Act - do whatever the PoisonIvey wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
