d1961ded3a79da5e7969c637995aba2da03fee97
341113090 <nathanthian@outlook.com>
2022-04-14 10:19:42 -0400

    Added plant label

--- Simulation-Project/MainWorld.java
+++ Simulation-Project/MainWorld.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.Random;
+import java.util.ArrayList;
 /**
  * Write a description of class MainWorld here.
  * 
@@ -13,6 +14,10 @@ public class MainWorld extends World
     private int numPoisonIvy;
     private int startNumCherry = 4;
     private int startNumPoisonIvy = 9;
+    private String[] plantLabels = new String[2];
+    Font funFont = new Font ("Comic Sans MS", false, false, 16);
+    Font boringFont = new Font ("Times New Roman", false, false, 14);
+    SuperTextBox bigPlantLabel;
     /**
      * Constructor for objects of class MainWorld.
      * 
@@ -21,11 +26,19 @@ public class MainWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        String cherryLabel = new String("Number of Cherries in the World: " + numCherry);
+        String poisonivyLabel = new String("Number of Poison Ivy in the World: " + numPoisonIvy);
+        plantLabels[0] = cherryLabel;
+        plantLabels[1] = poisonivyLabel;
+        bigPlantLabel = new SuperTextBox(plantLabels,boringFont,this.getWidth(),false); 
+        int tempY = bigPlantLabel.getImage().getHeight()/2;
+        int tempX = bigPlantLabel.getImage().getWidth()/2;
+        addObject(bigPlantLabel, tempX, tempY);
         for(int i = 0; i<startNumCherry; i++)
         {
             Random random = new Random();
             int xx = 50 + random.nextInt(500);
-            int yy = 50+ random.nextInt(300);
+            int yy = bigPlantLabel.getImage().getHeight()/2 + 30 + random.nextInt(300);
             addObject(new Cherry(), xx, yy);
         }
         for(int i = 0; i<startNumPoisonIvy; i++)
@@ -35,6 +48,37 @@ public class MainWorld extends World
             int yy = 50+ random.nextInt(300);
             addObject(new PoisonIvy(), xx, yy);
         }
-        addObject(new Herbivore(), 100, 100);
+    }
+    
+    /**
+     * In the act method, the world keeps track of how many cherries and ivies 
+     * there are.
+     */
+    public void act()
+    {
+        //Make an arraylist filled with cherries
+        ArrayList<Cherry> cherries = (ArrayList<Cherry>) this.getObjects(Cherry.class);
+        int numIterationsCherry = 0;
+        for(Cherry cherry : cherries)
+        {
+            //iterate through every cherry. For every cherry keep track of how many
+            numIterationsCherry++;
+        }
+        //current number of cherries is the number of iterations
+        numCherry = numIterationsCherry;
+        //Make an arraylist filled with ivy
+        ArrayList<PoisonIvy> ivies = (ArrayList<PoisonIvy>) this.getObjects(PoisonIvy.class);
+        int numIterationsIvy = 0;
+        for(PoisonIvy ivy : ivies)
+        {
+            //iterate through every ivy. For every ivy keep track of how many
+            numIterationsIvy++;
+        }
+        numPoisonIvy = numIterationsIvy;
+        String cherryLabel = new String("Number of Cherries in the World: " + numCherry);
+        String poisonivyLabel = new String("Number of Poison Ivy in the World: " + numPoisonIvy);
+        plantLabels[0] = cherryLabel;
+        plantLabels[1] = poisonivyLabel;
+        bigPlantLabel.update(plantLabels);
     }
 }
