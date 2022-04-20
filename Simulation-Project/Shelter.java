16fb098f55a9a66f4740b6add6b8cb767bbc7c90
maxtan84 <87951920+maxtan84@users.noreply.github.com>
2022-04-20 10:47:54 -0400

    documentation

--- Simulation-Project/Shelter.java
+++ Simulation-Project/Shelter.java
@@ -1,10 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Shelter here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * MAJOR THING FOR ANIMAL TO IMPLEMENT:     
+ * Animals that want to enter a shelter need to:
+ * <ol><br>
+ *      <li> Check if there is any animals in the shelter using getCurAnimals()</li>
+ *      <li> If there is: 
+ *      <ul> 
+ *          <li> Check what type of animal is there using getTypeAnimal()</li>
+ *          <li> Check if limit has been hit using limitHit()</li>
+ *      </ul>
+ *      <li> If there is not: </li>
+ *      <ul> 
+ *          <li> Animal needs to set the typeAnimal using setTypeAnimal</li>
+ *          <li> Animal needs to add one to the addCurAnimals()</li>
+ *      </ul>
+ * </ol>
+ * <br>
+ * 
+ * <p> Shelter comes in when the day ends and animals are finding a place to 
+ * stay for the night. There can only be one type of animal per shelter and there
+ * is a limit on how many animals can fit in one shelter. </p>
+ * 
+ * @author (Max) 
+ * @version (April 20th, 2022)
  */
 public class Shelter extends Actor
 {
@@ -12,26 +32,43 @@ public class Shelter extends Actor
      * Act - do whatever the Shelter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int limitAnimal;
+    private int limitAnimals;
+    private int curAnimals;
     private Animal typeAnimal;
-    private boolean isFull;
     
     public Shelter(){
-        limitAnimal = 2;
-        isFull = false;
+        limitAnimals = 2;
+        curAnimals = 0;
     }
     
     public void act()
     {
-        // Add your action code here.
+        
     }
     
+    /**
+     * This method checks when the limit of animals that can fit into a shelter.
+     */
     public boolean limitHit(){
-        if(isFull){
+        if(curAnimals == limitAnimals){
             return true;
         }
         return false;
     }
     
+    public Animal getTypeAnimal(){
+        return typeAnimal;
+    }
     
+    public void setTypeAnimal(Animal animal){
+        typeAnimal = animal;
+    }
+    
+    public int getCurAnimals(){
+        return curAnimals;
+    }
+    
+    public void addCurAnimals(){
+        curAnimals++;
+    }
 }
