d594ccf51a1d004b7d7c11c20d10adb868ed6689
frickingluwai <luwaiwong@gmail.com>
2022-04-21 09:49:31 -0400

    bruh3

--- Simulation-Project/Animal.java
+++ Simulation-Project/Animal.java
@@ -1,43 +1,42 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Animal here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Animal extends Animator
-{
-    public enum State{Waiting, Searching, Following, Attacking, Running}
-    
+public class Animal extends Animator {
+    public enum State {
+        Waiting, Searching, Following, Attacking, Running
+    }
+
     protected State state;
-    
+
     protected Actor target;
-    
+
     // Animal variables
     protected double speed; // Animal movement speed, increases more health decay when moving
-    protected int attack;  // Animal attack damage, decreases health
+    protected int attack; // Animal attack damage, decreases health
     protected int health; // Animal health/hp, decreases speed
     protected int size;
     protected double senseRange; // How far animal can detect threats/food, increases health decay
     protected double healthDecay; // How fast animals health goes down, hunger
     protected double altruism; // Chance of animal giving up its spot
-    
+
     // Attack variables
-    
-    
+
     /**
      * Act - do whatever the Animal wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act() {
         super.act();
         // Add your action code here.
-        
+
         // Super basic awful statemanager
-        
-        switch (state){
+
+        switch (state) {
             case Waiting:
                 Waiting();
                 break;
@@ -48,34 +47,33 @@ public class Animal extends Animator
                 Following();
                 break;
         }
-        
-        
+
         Behaviour();
     }
-    
-    protected void Behaviour(){
-        
+
+    protected void Behaviour() {
+
     }
-    
+
     ///// STATES /////
-    protected void Waiting(){
-        
+    protected void Waiting() {
+
     }
-    
-    protected void Searching(){
-        
+
+    protected void Searching() {
+
     }
-    
-    protected void Following(){
-        
+
+    protected void Following() {
+
     }
-    
-    protected void ReturnToShelter(){
-        
+
+    protected void ReturnToShelter() {
+
     }
-    
+
     ////// FUNCTIONS /////
-    protected void MoveTowardsObject(Actor target){
-        
+    protected void MoveTowardsObject(Actor target) {
+
     }
 }
