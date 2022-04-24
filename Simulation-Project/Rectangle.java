d43ab2d1263c41cbc0691e149ddb43ad744470f0
341113090 <nathanthian@outlook.com>
2022-04-24 15:57:31 -0400

    comments

--- Simulation-Project/Rectangle.java
+++ Simulation-Project/Rectangle.java
@@ -1,32 +1,39 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
 /**
- * Write a description of class Rectangle here.
+ * These rectangles are used in the title screen class to darken options 
+ * and also to detect for buttons
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Nathan Thian 
+ * @version April 24, 2022
  */
 public class Rectangle extends Actor
 {
+    //width and height of rectangles
     private int width = 0;
     private int height = 0;
+    
     /**
-     * Constructor for rectangle
+     * Constructor for rectangle.
+     * xx is the width
+     * yy is height
+     * trans is the transparency of the rectangle's image
      */
     public Rectangle(int xx, int yy, int trans)
     {
         this.width = xx;
         this.height = yy;
         GreenfootImage image = new GreenfootImage(width,height);
+        //all rectangles are black
         image.setColor(Color.BLACK);
         image.fillRect(0,0, width, height);
+        //can customize transparency desired
         image.setTransparency(trans);
         setImage(image);
     }
 
     /**
-     * Act - do whatever the Rectangle wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * The rectangle has no act method
      */
     public void act()
     {
